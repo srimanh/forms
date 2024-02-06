34a8a8fe0abff10facd3abdbdf2fa9482f59ba85
srimanh <srimandgl2004@gmail.com>
2024-02-06 16:44:44 +0530

    done changes

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import FormValidation from './Formvalidation'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <FormValidation/>
+  )
+}
+
+export default App
